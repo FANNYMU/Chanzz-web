9915feee888fc7c20a2cd61acc65c66682241af5
FANNYMU <154881121+FANNYMU@users.noreply.github.com>
2024-03-24 15:50:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("login-form");
+    const errorMessage = document.getElementById("error-message");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const username = form.username.value;
+        const password = form.password.value;
+
+        // Contoh validasi sederhana
+        if (username === "user" && password === "user") {
+            // Jika login berhasil, Anda dapat melakukan redirect ke halaman lain
+            window.location.href = "index.html";
+        } else {
+            // Jika login gagal, tampilkan pesan error
+            errorMessage.textContent = "Username atau password salah.";
+        }
+    });
+});
